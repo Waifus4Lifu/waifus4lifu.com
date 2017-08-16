2ca384f3cc4ff85d54fd8c870a1fdce2c010940e
Andrew LeCody <andrew.lecody@masergy.com>
2017-08-16 16:39:05 -0500

    Removed leading zeros from countdown

--- assets/scripts/countdown.js
+++ assets/scripts/countdown.js
@@ -1,5 +1,5 @@
 var nextYear = moment.tz("2017-08-23 12:00", "America/Chicago");
 
 $('#clock').countdown(nextYear.toDate(), function(event) {
-  $(this).html(event.strftime('%Dd %Hh %Mm %Ss'));
+  $(this).html(event.strftime('%-dd %-Hh %-Mm %-Ss'));
 });
